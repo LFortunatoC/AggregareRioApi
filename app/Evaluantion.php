c3abf4b930304824adc3d6c40adbb39fef3c2fee
Leandro Fortunato <fortunato.c77@gmail.com>
2020-02-14 21:51:54 -0500

    Update Evaluantion.php

--- app/Evaluantion.php
+++ app/Evaluantion.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Evaluantion extends Model
+class Evaluation extends Model
 {
     protected $fillable =['id','order_id', 'questionPoll_id','client_id','evaluationValue','comment'];
 }
