c4f1b91c310bc21cf7b32b5835f593c52810e258
julianaTamiazi <biju.juli@gmail.com>
2020-02-26 14:04:46 -0500

    searchs

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,8 @@ Route::/*middleware('auth:api')->*/patch('category/{id}', 'CategoryController@up
 Route::/*middleware('auth:api')->*/delete('category/{id}', 'CategoryController@destroy');
 Route::get('category', 'CategoryController@index');
 Route::get('category/{id}', 'CategoryController@show');
+Route::get('category/searchcategory/{language_id}', 'CategoryController@search');
+
 
 //Routes for SubCategory
 Route::/*middleware('auth:api')->*/post('subcategory', 'SubCategoryController@store');
@@ -30,6 +32,8 @@ Route::/*middleware('auth:api')->*/patch('subcategory/{id}', 'SubCategoryControl
 Route::/*middleware('auth:api')->*/delete('subcategory/{id}', 'SubCategoryController@destroy');
 Route::get('subcategory', 'SubCategoryController@index');
 Route::get('subcategory/{id}', 'SubCategoryController@show');
+Route::get('subcategory/searchsubcategory/{language_id}', 'SubCategoryController@search');
+
 
 //Routes for Language
 Route::/*middleware('auth:api')->*/post('language', 'LanguageController@store');
@@ -46,6 +50,21 @@ Route::/*middleware('auth:api')->*/delete('menu/{id}', 'MenuController@destroy')
 Route::get('menu', 'MenuController@index');
 Route::get('menu/{id}', 'MenuController@show');
 
+
+//Routes for Evaluation
+Route::/*middleware('auth:api')->*/post('evaluation', 'EvaluationController@store');
+Route::/*middleware('auth:api')->*/patch('evaluation/{id}', 'EvaluationController@update');
+Route::/*middleware('auth:api')->*/delete('evaluation/{id}', 'EvaluationController@destroy');
+Route::get('evaluation', 'EvaluationController@index');
+Route::get('evaluation/{id}', 'EvaluationController@show');
+
+//Routes for Order
+Route::/*middleware('auth:api')->*/post('order', 'OrderController@store');
+Route::/*middleware('auth:api')->*/patch('order/{id}', 'OrderController@update');
+Route::/*middleware('auth:api')->*/delete('order/{id}', 'OrderController@destroy');
+Route::get('order', 'OrderController@index');
+Route::get('order/{id}', 'OrderController@show');
+
 //Routes for Item
 Route::/*middleware('auth:api')->*/post('item', 'ItemController@store');
 Route::/*middleware('auth:api')->*/patch('item/{id}', 'ItemController@update');
@@ -53,6 +72,7 @@ Route::/*middleware('auth:api')->*/delete('item/{id}', 'ItemController@destroy')
 Route::get('item', 'ItemController@index');
 Route::get('item/{id}', 'ItemController@show');
 
+//Routes for Item Title Desc
 //Routes for Promotion
 Route::/*middleware('auth:api')->*/post('promotion', 'PromotionController@store');
 Route::/*middleware('auth:api')->*/patch('promotion/{id}', 'PromotionController@update');
@@ -60,10 +80,26 @@ Route::/*middleware('auth:api')->*/delete('promotion/{id}', 'PromotionController
 Route::get('promotion', 'PromotionController@index');
 Route::get('promotion/{id}', 'PromotionController@show');
 
+
 //Routes for ItemTitleDesc
 Route::/*middleware('auth:api')->*/post('itemtitledesc', 'ItemTitleDescriptionController@store');
 Route::/*middleware('auth:api')->*/patch('itemtitledesc/{id}', 'ItemTitleDescriptionController@update');
 Route::/*middleware('auth:api')->*/delete('itemtitledesc/{id}', 'ItemTitleDescriptionController@destroy');
 Route::get('itemtitledesc', 'ItemTitleDescriptionController@index');
 Route::get('itemtitledesc/{id}', 'ItemTitleDescriptionController@show');
+Route::get('itemtitledesc/searchtitledesc/{language_id}', 'ItemTitleDescriptionController@search');
 
+
+//Routes for Menu Title Desc
+Route::/*middleware('auth:api')->*/post('menutitledesc', 'MenuTitleDescController@store');
+Route::/*middleware('auth:api')->*/patch('menutitledesc/{id}', 'MenuTitleDescController@update');
+Route::/*middleware('auth:api')->*/delete('menutitledesc/{id}', 'MenuTitleDescController@destroy');
+Route::get('menutitledesc', 'MenuTitleDescController@index');
+Route::get('menutitledesc/{id}', 'MenuTitleDescController@show');
+
+//Routes for Question Pool
+Route::/*middleware('auth:api')->*/post('questionpool', 'QuestionPoolController@store');
+Route::/*middleware('auth:api')->*/patch('questionpool/{id}', 'QuestionPoolController@update');
+Route::/*middleware('auth:api')->*/delete('questionpool/{id}', 'QuestionPoolController@destroy');
+Route::get('questionpool', 'QuestionPoolController@index');
+Route::get('questionpool/{id}', 'QuestionPoolController@show');
